a0bfbe5de14f150ae477793d897530283e32aa88
YANAGAWA <YANA@jp.ibm.com>
2017-10-15 18:55:16 +0900

    test

--- hello.py
+++ hello.py
@@ -3,7 +3,11 @@ import sys
 args = sys.argv
 
 print args
-dec = args[1] + "_decode"
-print (dec)
+dec = args[1]
+if dec == "aaa":
+    print ("yes")
+else:
+    print ("no")
+
 print ("hello")
 print ("hoge")
